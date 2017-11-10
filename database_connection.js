3f4d41239626ff7876bf181e1270d5870b1bc2a7
Josh <leong.jo@gmail.com>
2017-11-10 15:29:19 -0700

    preheroku migrate

--- database_connection.js
+++ database_connection.js
@@ -1,11 +1,24 @@
 require("dotenv").load();
 
+var database_connection;
+
+switch (process.env.NODE_ENV){
+    case "production":
+        database_connection = process.env.DATABASE_URL;
+    break;
+    default;
+        default:
+            database_connection = {
+                database: process.env.DATABASE_NAME,
+                user: process.env.DATABASE_USER,
+                password: process.env.DATABASE_PASSWORD
+            }
+        break;
+}
+
 var configuration = {
     client: "postgresql",
-    connection: {
-        database: process.env.DATABASE_NAME,
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD
-    }
+    connection: database_connection
+
 };
 module.exports = require("knex")(configuration);
--- knexfile.js
+++ knexfile.js
@@ -8,5 +8,9 @@ module.exports = {
                     user:     process.env.DATABASE_USER,
                     password: process.env.DATABASE_PASSWORD
             }
+    },
+    production: {
+        client: "postgresql",
+        connection: process.env.DATABASE_URL
     }
 };
