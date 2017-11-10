5b9d66e11c7b38e909c34423cf751c00be3cee89
Josh <leong.jo@gmail.com>
2017-11-10 13:46:34 -0700

    fixing seds

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./routes/books');
 
 var app = express();
+
+var routes = require("./routes/index");
+var books = require("./routes/books");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,7 +33,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/books', books);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ knexfile.js
@@ -0,0 +1,12 @@
+require("dotenv").load();
+
+module.exports = {
+    development: {
+            client: "postgresql",
+            connection: {
+                    database: process.env.DATABASE_NAME,
+                    user:     process.env.DATABASE_USER,
+                    password: process.env.DATABASE_PASSWORD
+            }
+    }
+};
